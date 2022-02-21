c051f12154302b7cf69442991f3f8ee60d76f2e0
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2022-02-21 17:18:10 +0200

    version change

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ class Config extends ConfigCore
 {
     const NAME        = 'buckaroo3';
     const PLUGIN_NAME = 'Buckaroo Payments';
-    const VERSION     = '3.3.11';
+    const VERSION     = '3.4.0';
     //ATTENTION: If log is enabled it can be potential vulnerability
     const LOG = false;
 
