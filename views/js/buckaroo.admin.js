f9da1985c6e2e0b725edd2c730115c63f93da7ad
vegim carkaxhija <vegim.carkaxhija@gmail.com>
2023-05-24 10:59:07 +0200

    refactor code

--- views/js/buckaroo.admin.js
+++ views/js/buckaroo.admin.js
@@ -14,6 +14,7 @@
  */
 $(document).ready(function () {
     $(".hidable.disabled").hide();
+
     $(".enabledisable").change(function () {
         if ($(this).val() == 0) {
             $(this).parents("fieldset").children("legend").removeClass("test").removeClass("active");
@@ -24,7 +25,6 @@ $(document).ready(function () {
             } else {
                 $(this).parents("fieldset").children("legend").addClass("test");
             }
-            ;
 
             $(this).parents("fieldset").children(".hidable").show();
         }
@@ -37,43 +37,31 @@ $(document).ready(function () {
         }
     });
 
-    // Check the initial state of the toggle switches
     $(".toggle-switch").each(function() {
-        var panelID = this.dataset.target;
-
-        // Check if the switch is enabled (checked)
         if ($(this).is(":checked")) {
-            // Remove the "collapse" class to keep the panel open
-            $(panelID).collapse('toggle');
+            togglePanel.call(this);
         }
-    });
-    // Listen for the change event on switches
-    $(".toggle-switch").change(function() {
-        // Get the ID of the panel associated with this switch
+    }).change(togglePanel);
+
+    function togglePanel() {
         var panelID = this.dataset.target;
-        // Check if the switch is enabled (checked)
         $(panelID).collapse('toggle');
-    });
-    $( function() {
-        $( "#sortable" ).sortable();
-        $( "#sortable" ).disableSelection();
-    } );
+    }
+
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
 
     function updatePositions() {
-        // Iterate over each sortable item
         $("#sortable .panel").each(function(index) {
-            // Update the value of the hidden input field to reflect the current index
             $(this).find('.position-input').val(index);
         });
     }
 
     $("#sortable").sortable({
-        update: function(event, ui) {
-            // Call the function to update positions whenever a sort operation is performed
+        update: function() {
             updatePositions();
         }
     });
 
-    // Also call the function on page load to initialize positions
     updatePositions();
 });
