5e86f1ec7792b36539e7aa137ba6b715e2508a88
vegim carkaxhija <vegim.carkaxhija@gmail.com>
2023-07-27 12:39:11 +0200

    add index.php through autoindex

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -114,8 +114,8 @@ class ComposerStaticInitd62d14618d03e0713a1baeaa97b087ba
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
