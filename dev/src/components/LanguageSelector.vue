2baf663ce7f1fb9c3fffd8bbce0c5fc7c5e7b0de
Rinor12010 <105772190+Rinor12010@users.noreply.github.com>
2023-11-24 15:07:33 +0100

    BP-3134-Add-language-files-fr-de

--- dev/src/components/LanguageSelector.vue
+++ dev/src/components/LanguageSelector.vue
@@ -2,15 +2,10 @@
     <div class="md:px-6 w-full text-white text-sm relative">
         <div>
             <div class="inline-block hover:bg-sixthly p-2 cursor-pointer rounded-lg" @click="showMenu = !showMenu">
-                <div v-if="currentLanguage === 'en'" class="flex space-x-1">
-                    <img src="../../../../../img/flags/gb.jpg" class="w-4" alt=""/>
-                    <span class="text-xs">English <i class="fas fa-chevron-down text-[8px]"></i></span>
-                </div>
-
-                <div v-if="currentLanguage === 'nl'" class="flex space-x-1">
-                    <img src="../../../../../img/flags/nl.jpg" class="w-4" alt=""/>
-                    <span class="text-xs">Nederlands <i class="fas fa-chevron-down text-[8px]"></i></span>
-                </div>
+              <div v-for="({name,flag}) in filterCurrentLanguage(languages)" class="flex space-x-1">
+                  <img :src="'../../../../../img/flags/'+flag+'.jpg'" class="w-4" alt=""/>
+                  <span class="text-xs">{{name}} <i class="fas fa-chevron-down text-[8px]"></i></span>
+              </div>
             </div>
         </div>
 
@@ -21,8 +16,10 @@
                     leave-from-class="opacity-100 translate-y-0"
                     leave-to-class="opacity-0 translate-y-3">
             <ul v-if="showMenu" ref="languageMenuRef" class="bg-white text-gray-800 rounded-lg inline-block shadow-xl mt-1 absolute w-1/2 overflow-hidden">
-                <li v-if="currentLanguage !== 'nl'" @click="changeLanguage('nl')" class="p-2 flex space-x-2 cursor-pointer hover:bg-gray-200"><img src="../../../../../img/flags/nl.jpg" alt="" class="w-4"/> <div>Nederlands</div></li>
-                <li v-if="currentLanguage !== 'en'" @click="changeLanguage('en')" class="p-2 flex space-x-2 cursor-pointer hover:bg-gray-200"><img src="../../../../../img/flags/gb.jpg" alt="" class="w-4"/> <div>English</div></li>
+              <li v-for="({name,flag,code}) in filterCurrentLanguage(languages,false)" @click="changeLanguage(code)" class="p-2 flex space-x-2 cursor-pointer hover:bg-gray-200">
+                  <img :src="'../../../../../img/flags/'+flag+'.jpg'"  alt="" class="w-4"/>
+                  <div>{{name}}</div>
+              </li>
             </ul>
         </Transition>
     </div>
@@ -40,20 +37,46 @@ export default {
     const showMenu = ref(false);
     const languageMenuRef = ref(null);
     const currentLanguage = ref(locale.value);
-
     onClickOutside(languageMenuRef, () => (showMenu.value = false));
-
+    const languages = [
+      {
+        code: "en",
+        name: "English",
+        flag: "gb"
+      },
+      {
+        code: "nl",
+        name: "Dutch",
+        flag: "nl"
+      },
+      {
+        code: "de",
+        name: "German",
+        flag: "de"
+      },
+      {
+        code: "fr",
+        name: "French",
+        flag: "fr"
+      }
+    ]
+    const filterCurrentLanguage = (obj,include = true) => {
+      return obj.filter((item) => {
+        return include ? item.code === currentLanguage.value : item.code !== currentLanguage.value
+      });
+    }
     const changeLanguage = (lang) => {
       locale.value = lang;
       showMenu.value = false;
       currentLanguage.value = lang;
     };
-
     return {
       showMenu,
       languageMenuRef,
       currentLanguage,
       changeLanguage,
+      languages,
+      filterCurrentLanguage
     };
   },
 };
