d167d7b1aeffe1420831424ec8cbde16aa1743b8
vegim carkaxhija <vegim.carkaxhija@gmail.com>
2024-06-18 15:13:23 +0200

    update

--- library/checkout/billinkcheckout.php
+++ library/checkout/billinkcheckout.php
@@ -126,8 +126,8 @@ class BillinkCheckout extends Checkout
             $article = [
                 'identifier' => $item['id_product'],
                 'quantity' => $item['quantity'],
-                'price' => (new DecimalNumber((string) $item['price']))->toPrecision(2),
-                'priceExcl' => (new DecimalNumber((string) $item['price_wt']))->toPrecision(2),
+                'price' => (new DecimalNumber((string) $item['price_wt']))->toPrecision(2),
+                'priceExcl' => (new DecimalNumber((string) $item['price']))->toPrecision(2),
                 'vatPercentage' => $item['rate'],
                 'description' => $item['name'],
             ];
