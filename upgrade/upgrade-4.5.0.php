caced72c23a155f8a12fdef2d823eefeb1404fc4
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-04-07 13:14:24 +0200

    update payment method name from "sofort" to "sofortueberweisung" in upgrade script

--- upgrade/upgrade-4.5.0.php
+++ upgrade/upgrade-4.5.0.php
@@ -28,7 +28,7 @@ if (!defined('_PS_VERSION_')) {
 function upgrade_module_4_5_0($object)
 {
     Db::getInstance()->execute('DELETE FROM ' . _DB_PREFIX_ . 'bk_payment_methods WHERE name = "giropay"');
-    Db::getInstance()->execute('DELETE FROM ' . _DB_PREFIX_ . 'bk_payment_methods WHERE name = "sofort"');
+    Db::getInstance()->execute('DELETE FROM ' . _DB_PREFIX_ . 'bk_payment_methods WHERE name = "sofortueberweisung"');
     Db::getInstance()->execute('UPDATE ' . _DB_PREFIX_ . 'bk_payment_methods SET label = "goSettle" WHERE name = "knaken"');
 
     $orderingRepository = new RawOrderingRepository();
