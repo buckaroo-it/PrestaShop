8fbbf2c25bc9178dabc5bb7dc0094a4161d39835
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-05-14 13:59:54 +0200

    fix: update target element for error message display in payment process

--- views/js/buckaroo-error.js
+++ views/js/buckaroo-error.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!window.buckaroo_error_msg) return;
 
     const wrapper = document.createElement('div');
-    wrapper.className = 'container js-buckaroo-payment-error my-3';
+    wrapper.className = 'container js-buckaroo-payment-error';
 
     wrapper.innerHTML = `
     <article class="alert alert-danger" role="alert" data-alert="danger">
